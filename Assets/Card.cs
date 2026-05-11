4d019bdc525677fbb37113fccd3c713c82c95aba
coffeeisok <3366257117@qq.com>
2026-05-11 18:31:10 +0800

    5.11

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 /*
 植物卡片功能脚本
 三种状态：
@@ -8,9 +9,21 @@ using UnityEngine;
 2. 阳光充足
 3. 阳光不足 
 */
-
+public enum CardState
+{
+    CD,
+    NoSun,
+    Ready
+}
 public class Card : MonoBehaviour
 {
+    public CardState state;
+    // 定义这三张图片
+    public Button ready;
+    public GameObject NoSun;
+    
+    public Image cdImg;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,13 +34,22 @@ public class Card : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (state)
+       switch (state)
         {
             case CardState.CD:
+                ready.gameObject.SetActive(true);
+                NoSun.gameObject.SetActive(true);
+                cdImg.gameObject.SetActive(true);
                 break;
             case CardState.NoSun:
+                ready.gameObject.SetActive(true);
+                NoSun.gameObject.SetActive(true);
+                cdImg.gameObject.SetActive(false);
                 break;
-            case CardState.ready:
+            case CardState.Ready:
+                ready.gameObject.SetActive(true);
+                NoSun.gameObject.SetActive(false);
+                cdImg.gameObject.SetActive(false);
                 break;
         }
     }
